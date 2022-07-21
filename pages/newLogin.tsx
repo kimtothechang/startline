f3f99385e99ad035471742d14eb502f14fd9737b
chango <kimtothechang@gmail.com>
2022-07-21 15:48:08 +0900

    test: make testpage

--- pages/newLogin.tsx
+++ pages/newLogin.tsx
@@ -2,6 +2,9 @@ import { NextPage } from "next";
 import { useForm } from "react-hook-form";
 import styled from '@emotion/styled'
 import useMutation from "../libs/client/useMutation";
+import Layout from '../components/layout';
+import Header from '../components/header';
+
 
 interface LoginForm {
     id:string;
@@ -18,11 +21,15 @@ const NewLogin:NextPage = () => {
         login(valuse);
     }
 
-    return <form onSubmit={handleSubmit(onValid)}>
-        <Input type="text" {...register('id')}/>
-        <Input type="text" {...register('pw')}/>
-        <button>login</button>
-    </form>
+    return <Layout isHeader isNavBar Header={<Header text="newLogin" />}>
+        <form onSubmit={handleSubmit(onValid)}>
+            <Input type="text" {...register('id')}/>
+            <Input type="text" {...register('pw')}/>
+            <button>login</button>
+        </form>
+    </Layout>
+    
+    
 }
 
 export default NewLogin;
